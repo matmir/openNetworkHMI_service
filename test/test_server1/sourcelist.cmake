d9d0940320564a438e014cbb842006dba7f52a3d
Mateusz Miroslawski <mateusz@s-mail.pl>
2020-12-18 10:36:51 +0100

    Update build system

---
+++ test/test_server1/sourcelist.cmake
@@ -0,0 +1,12 @@
+# Source files
+
+target_sources(${PROJECT_NAME} PRIVATE
+    "src/onh_test_server1.cpp"
+	"src/Application.cpp"
+	"src/utils/Exception.cpp"
+	"src/utils/Exception.h"
+	"src/utils/Delay.cpp"
+	"src/utils/Delay.h"
+	"src/Application.h"
+	"src/controlBits/serverControlBits_test.h"
+)
---
+++ test/test_server2/sourcelist.cmake
@@ -0,0 +1,10 @@
+# Source files
+
+target_sources(${PROJECT_NAME} PRIVATE
+    "src/Application.cpp"
+	"src/onh_test_server2.cpp"
+	"src/utils/Exception.cpp"
+	"src/utils/Exception.h"
+	"src/Application.h"
+	"src/controlBits/modbusControlBits_test.h"
+)
